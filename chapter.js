eede6f88b1690ebc962b8bf9fbcdb212caa1c3b0
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-02-02 18:13:30 +0300

    Modified the styling a little bit

--- chapter.js
+++ chapter.js
@@ -1,13 +1,14 @@
 export default {
-    props: {
-        name: String,
-        examples: Array, // [ { name: String, iframe: 'https...' } ]
-    },
-    template: `
-    <section class="space-y-4">
+  props: {
+    name: String,
+    examples: Array, // [ { name: String, iframe: 'https...' } ]
+  },
+  template: `
+    <section class="space-y-4 mb-12">
+        <h2 class="text-3xl font-bold tracking-wide mb-8">{{ name }}</h2>
         <iframe
             v-for="example in examples"
-            class="w-full border-none transition-opacity opacity-100"
+            class="w-full border rounded-2xl"
             loading="lazy"
             :src="example.iframe"
             style="aspect-ratio: 2.66667 / 1"
